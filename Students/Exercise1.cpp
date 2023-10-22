7fbc7e693a275946b7f32a40a25bf572a89fefc3
RickyTan5350 <147962052+RickyTan5350@users.noreply.github.com>
2023-10-22 09:15:13 +0000

    TANLIJI

---
+++ Students/Exercise1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int x;
+    double y;
+    char z;
+    char a[21];
+    cout<< "Enter an integer: ";
+    cin>> x ;
+    cout<< "Enter a decimal number: ";
+    cin>> y ;
+    cout<< "Enter a single character: ";
+    cin>> z ;
+    cout<< "Enter an array of character: ";
+    cin>> a ;
+    return 0;
+    
+
+}
